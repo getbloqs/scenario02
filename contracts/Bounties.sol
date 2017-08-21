2ab0dd35ed2f219e701253c821fe1371e982fe0e
Stefan Huber <stefan.huber@beyondit.at>
2017-08-21 14:05:36 +0200

    chore: init

---
+++ contracts/Bounties.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+contract Bounties {
+    
+    struct Bounty {
+        mapping (address => uint) donations;
+        uint totalAmount;
+        uint totalDonators;
+    }
+
+    mapping (string => Bounty) bounties;
+
+    function addBounty(string issueLink) payable returns (bool) {
+        if (msg.value > 0) {
+            if (bounties[issueLink].donations[msg.sender] <= 0) {
+                bounties[issueLink].totalDonators += 1;
+            }
+            bounties[issueLink].totalAmount += msg.value;
+            bounties[issueLink].donations[msg.sender] += msg.value;
+        }
+
+        return false;        
+    }
+
+}
