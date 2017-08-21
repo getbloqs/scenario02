fed55ee8cf46449d2618d9bb165212b9f6eb716c
Stefan Huber <stefan.huber@beyondit.at>
2017-08-21 15:58:40 +0200

    chore: other functions

--- contracts/Bounties.sol
+++ contracts/Bounties.sol
@@ -1,27 +1,40 @@
 pragma solidity ^0.4.11;
 
 contract Bounties {
+
+    enum BountyState { Open, Closed }
+
+    event BountyClaim(string issueLink, string solutionLink, address claimant);
     
     struct Bounty {
         mapping (address => uint) donations;
+
+        BountyState state;
+
         uint totalAmount;
         uint totalDonators;
     }
 
     mapping (string => Bounty) bounties;
 
-    function addBounty(string issueLink) payable returns (bool) {
-        if (msg.value > 0) {
-            if (bounties[issueLink].donations[msg.sender] <= 0) {
-                bounties[issueLink].totalDonators += 1;
-            }
-            bounties[issueLink].totalAmount += msg.value;
-            bounties[issueLink].donations[msg.sender] += msg.value;
+    function addBounty(string issueLink) payable {
+        require(msg.value > 0 && bytes(issueLink).length > 0);
 
-            return true;
+        if (bounties[issueLink].donations[msg.sender] <= 0) {
+            bounties[issueLink].totalDonators += 1;
         }
+        bounties[issueLink].totalAmount += msg.value;
+        bounties[issueLink].donations[msg.sender] += msg.value;
+    }
 
-        return false;        
+    function claimBounty(string issueLink, string solutionLink) {
+        require(bytes(solutionLink).length > 0);
+
+
+    }
+
+    function acceptClaim(string issueLink, string solutionLink) {
+
     }
 
 }
